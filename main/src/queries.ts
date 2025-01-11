9d8ca8afb880918fe06dae9874fe23b0b34c299a
Jen Thompson <writejthompson@gmail.com>
2025-01-10 21:09:18 -0600

    Update queries.ts
    removed the console.log for the .env info that i had created when things weren't working

--- main/src/queries.ts
+++ main/src/queries.ts
@@ -13,13 +13,6 @@ db.connect()
   .then(() => console.log("Database connected!"))
   .catch((err) => console.error("Database connection error:", err));
 
-  console.log({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-  });
 
 export const getAllDepartments = async () => {
   const query = "SELECT id, name FROM department;";
